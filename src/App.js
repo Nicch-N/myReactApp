dbd4b5ee454f1e82016dc1296668f425a2160c9f
Nicch-N <Nicch-N@users.noreply.github.com>
2022-09-27 16:52:21 +0700

    Added button to change rotating atom speed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+function SpinningLogo(props){
+  if(props.speed===0){
+    return (<img src={logo} className="App-logo" alt="logo" />);
+  }else{
+    return (<img src={logo} className="App-logo2" alt="logo" />);
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rotating atom bullshit
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React here mf
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {speed: 0};
+  }
+
+  handleClick() {
+    if(this.state.speed==0){
+      this.setState({ speed:1 }) 
+    }else{
+      this.setState({ speed:0 }) 
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SpinningLogo speed={this.state.speed}/>
+          <p>
+            Rotating atom bullshit
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React here mf
+          </a>
+          <button className='btn bg-primary' onClick={this.handleClick.bind(this)}>Click!</button>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ import './NavBar.css';
 function NavBar() {
   return (
     <nav className="navbar ">
-      <a href='App.js' className=' link-dark nav-item mx-3'>Home</a>
+      <a href='./App2.js' className=' link-dark nav-item mx-3'>Home</a>
     </nav>
   );
 }
