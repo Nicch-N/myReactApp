1a60cd26f55658302f4ff08ac16ee0efdd8a8c56
Nicch-N <Nicch-N@users.noreply.github.com>
2022-09-27 15:45:14 +0700

    Some changes

---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <nav className="navbar ">
+      <a href='App.js' className=' link-dark nav-item mx-3'>Home</a>
+    </nav>
+  );
+}
+
+export default NavBar;
